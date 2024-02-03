130c290f439bad4babc5f787ce18148cf6d62d77
Sarvesh Narayanan <74053896+nsarvesh2011@users.noreply.github.com>
2024-02-03 12:37:34 -0500

    Cart Page
    Added Cart Page and added remove from cart functionality

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -6,7 +6,7 @@ export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
     let cart = {};
-    for (let index = 0; index < all_product.length; index++) {
+    for (let index = 0; index < all_product.length+1; index++) {
         cart[index] = 0;    
     }
     return cart;
@@ -14,7 +14,17 @@ const getDefaultCart = () => {
 
 const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
-    const contextValue = {all_product}
+
+    const addToCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        console.log(cartItems);
+    }
+    const removeFromCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
+
+
+    const contextValue = {all_product, cartItems, addToCart, removeFromCart};
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
